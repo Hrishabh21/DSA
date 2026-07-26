c46d4c9df5cbcfa2b1df93e18a520cc4abb8016a
Hrishabh Gupta <118746244+Hrishabh21@users.noreply.github.com>
2026-07-27 00:21:04 +0530

    Add Java Solution to Patterns/Arrays

--- LeetCode/Patterns/Arrays/854. Making A Large Island/Solution.java
+++ LeetCode/Patterns/Arrays/854. Making A Large Island/Solution.java
@@ -79,8 +79,8 @@ class Solution {
                     if(isValid(adr,adc,n,n)&&grid[adr][adc]==1){
                         f=true;
                         for(int l =k+1;l<4;l++){
-                            int aadc = j+dr[l];
-                            int aadr = i+dc[l];
+                            int aadr = j+dr[l];
+                            int aadc = i+dc[l];
                             int and = aadr*n+aadc;
                             if(isValid(aadr,aadc,n,n)&&grid[aadr][aadc]==1){
                                  if(dsu.findParent(nd)!= dsu.findParent(and)){
