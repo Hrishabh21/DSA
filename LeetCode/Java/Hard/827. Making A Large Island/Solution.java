ac3d65235fb0e8c94a1778c92ef2f77881d065b2
Hrishabh Gupta <118746244+Hrishabh21@users.noreply.github.com>
2026-07-27 00:16:40 +0530

    Add Hard Java Solution from LeetCode

--- LeetCode/Java/Hard/827. Making A Large Island/Solution.java
+++ LeetCode/Java/Hard/827. Making A Large Island/Solution.java
@@ -87,12 +87,15 @@ class Solution {
                                 ans = Math.max(dsu.size[dsu.findParent(nd)]+dsu.size[dsu.findParent(and)]+1,ans);
 
                             }
-                            
-                            }
                             else{
                                 ans = Math.max(dsu.size[dsu.findParent(nd)]+1,ans);
                             }
+                            }
+                            
                         }
+                        
+                                ans = Math.max(dsu.size[dsu.findParent(nd)]+1,ans);
+                            
 
             }           
         }
