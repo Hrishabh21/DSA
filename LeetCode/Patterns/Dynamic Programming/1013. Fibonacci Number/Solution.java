f003df62104625c74ec9a95ac843a2acf8b9f139
Hrishabh Gupta <118746244+Hrishabh21@users.noreply.github.com>
2026-07-28 11:18:27 +0530

    Add Java Solution to Patterns/Dynamic Programming

--- LeetCode/Patterns/Dynamic Programming/1013. Fibonacci Number/Solution.java
+++ LeetCode/Patterns/Dynamic Programming/1013. Fibonacci Number/Solution.java
@@ -1,12 +1,8 @@
 class Solution {
     public int fib(int n) {
-         if(n<2) return n;
-        int[] dp = new int[n+1];
-        Arrays.fill(dp,-1);
-        dp[0] = 0;
-        dp[1] = 1;
-        if(dp[n]!=-1) return dp[n];
-        return dp[n] = fib(n-1)+fib(n-2);
-       
+        if(n==0) return 0;
+        if(n==1) return 1;
+        return fib(n-1)+fib(n-2);
+        
     }
 }
