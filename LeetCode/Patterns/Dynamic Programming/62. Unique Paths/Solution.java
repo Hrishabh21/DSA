aff2424e846209d019024eca00de82028f73ca08
Hrishabh Gupta <118746244+Hrishabh21@users.noreply.github.com>
2026-07-30 13:22:11 +0530

    Add Java Solution to Patterns/Dynamic Programming

--- LeetCode/Patterns/Dynamic Programming/62. Unique Paths/Solution.java
+++ LeetCode/Patterns/Dynamic Programming/62. Unique Paths/Solution.java
@@ -12,18 +12,23 @@ class Solution {
 
     // }
     public int uniquePaths(int m, int n) {
-    int dp[][] = new int[m+1][n+1];
+    int dp[] = new int[n];
     
-   // travel(0,0,dp,m,n);
-   for(int i=1;i<n+1;i++){
-    dp[1][i] =1; 
-   }
-   for(int i =2;i<m+1;i++){
-    for(int j =1;j<n+1;j++){
-        dp[i][j] = dp[i-1][j]+dp[i][j-1];
+    Arrays.fill(dp,1);
+
+    // for(int i=1;i<n+1;i++){
+    //  dp[1][i] =1; 
+    // }
+
+    for(int i =1;i<m;i++){
+        int dp2[] = new int[n];
+        dp2[0] = 1;
+    for(int j =1;j<n;j++){
+        dp2[j] = dp[j]+dp2[j-1];
+     }
+     dp = dp2.clone();
     }
-   }
-   return dp[m][n];
+    return dp[n-1];
 
 
       
