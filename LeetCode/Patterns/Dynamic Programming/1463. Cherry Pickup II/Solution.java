a622097b6a1f474ab0b53e8a36d6a50c5f76060c
Hrishabh Gupta <118746244+Hrishabh21@users.noreply.github.com>
2026-08-06 20:40:17 +0530

    Add Java Solution to Patterns/Dynamic Programming

---
+++ LeetCode/Patterns/Dynamic Programming/1463. Cherry Pickup II/Solution.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int rec(int i,int j1,int j2,int m,int n,int grid[][]){
+        if(j1<0||j2<0||j1>n-1||j2>n-1){
+            return (int)-1e9;
+        }
+        if(i == m-1){
+            if(j1==j2) return grid[i][j1];
+            else return grid[i][j1]+grid[i][j2];
+        }
+        int max = (int)-1e9;
+        int curr = (j1==j2)?grid[i][j1]:grid[i][j2]+grid[i][j1];
+
+        for(int k = -1;k<2;k++){
+            for(int l = -1;l<2;l++){
+                int ans = curr+rec(i+1,j1+k,j2+l,m,n,grid);
+                max = Math.max(ans,max);
+
+            }
+        }
+        return max;
+    }
+    public int cherryPickup(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+       // int[][][] dp = new int[m][n][n];
+      return rec(0,0,n-1,m,n,grid);
+        
+
+       
+        
+    }
+}
