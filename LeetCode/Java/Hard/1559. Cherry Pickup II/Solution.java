3bd5bf0b67072dfb22b60167bd7706fdfe558cf4
Hrishabh Gupta <118746244+Hrishabh21@users.noreply.github.com>
2026-08-06 21:55:27 +0530

    Add Hard Java Solution from LeetCode

--- LeetCode/Java/Hard/1559. Cherry Pickup II/Solution.java
+++ LeetCode/Java/Hard/1559. Cherry Pickup II/Solution.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int rec(int i,int j1,int j2,int m,int n,int grid[][]){
+    public int rec(int i,int j1,int j2,int m,int n,int dp[][][],int grid[][]){
         if(j1<0||j2<0||j1>n-1||j2>n-1){
             return (int)-1e9;
         }
@@ -7,23 +7,31 @@ class Solution {
             if(j1==j2) return grid[i][j1];
             else return grid[i][j1]+grid[i][j2];
         }
+        if(dp[i][j1][j2]!=-1) return dp[i][j1][j2];
         int max = (int)-1e9;
         int curr = (j1==j2)?grid[i][j1]:grid[i][j2]+grid[i][j1];
 
         for(int k = -1;k<2;k++){
             for(int l = -1;l<2;l++){
-                int ans = curr+rec(i+1,j1+k,j2+l,m,n,grid);
+                int ans = curr+rec(i+1,j1+k,j2+l,m,n,dp,grid);
                 max = Math.max(ans,max);
 
             }
         }
-        return max;
+        return dp[i][j1][j2]= max;
     }
     public int cherryPickup(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-       // int[][][] dp = new int[m][n][n];
-      return rec(0,0,n-1,m,n,grid);
+        int[][][] dp = new int[m][n][n];
+
+       for(int[][]a : dp){
+        for(int[]b:a){
+            Arrays.fill(b,-1);
+        }
+       }
+
+      return rec(0,0,n-1,m,n,dp,grid);
         
 
        
