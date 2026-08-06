f732802f65592030032710e3ef65628ef967aad9
Hrishabh Gupta <118746244+Hrishabh21@users.noreply.github.com>
2026-08-06 22:13:07 +0530

    Add Java Solution to Patterns/Dynamic Programming

--- LeetCode/Patterns/Dynamic Programming/1559. Cherry Pickup II/Solution.java
+++ LeetCode/Patterns/Dynamic Programming/1559. Cherry Pickup II/Solution.java
@@ -25,16 +25,46 @@ class Solution {
         int n = grid[0].length;
         int[][][] dp = new int[m][n][n];
 
-       for(int[][]a : dp){
-        for(int[]b:a){
-            Arrays.fill(b,-1);
+    //    for(int[][]a : dp){
+    //     for(int[]b:a){
+    //         Arrays.fill(b,-1);
+    //     }
+    //    }
+
+    //   return rec(0,0,n-1,m,n,dp,grid);
+
+    for(int j1 =0;j1<n;j1++){
+        for(int j2 =0;j2<n;j2++){
+            if(j1 == j2) dp[m-1][j1][j2] = grid[m-1][j1];
+            else dp[m-1][j1][j2] = grid[m-1][j1]+grid[m-1][j2];
+
         }
-       }
+    }
 
-      return rec(0,0,n-1,m,n,dp,grid);
-        
-
-       
-        
+    for(int i = m-2;i>=0;i--){
+        for(int j1 =0;j1<n;j1++){
+            for(int j2 =0;j2<n;j2++){
+                    int maxi = (int)(-1e9);
+                    int curr = (j1 == j2) ? grid[i][j1] 
+                                          : grid[i][j1] + grid[i][j2];
+                    // Try all 9 moves
+                    for (int dj1 = -1; dj1 <= 1; dj1++) {
+                        for (int dj2 = -1; dj2 <= 1; dj2++) {
+                            int newJ1 = j1 + dj1;
+                            int newJ2 = j2 + dj2;
+                            if (newJ1 >= 0 && newJ1 < n &&
+                                newJ2 >= 0 && newJ2 < n) {
+                                maxi = Math.max(maxi, curr + 
+                                       dp[i+1][newJ1][newJ2]);
+                            } else {
+                                maxi = Math.max(maxi, (int)(-1e9));
+                            }
+                        }
+                    }
+                    dp[i][j1][j2] = maxi;
+                }
+            }
+        }
+        return dp[0][0][n-1];
     }
 }
