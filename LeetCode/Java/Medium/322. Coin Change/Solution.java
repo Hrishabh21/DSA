56dbc47fe38e728a8df0b83ab97594332187bfc7
Hrishabh Gupta <118746244+Hrishabh21@users.noreply.github.com>
2026-08-16 21:08:04 +0530

    Add Medium Java Solution from LeetCode

--- LeetCode/Java/Medium/322. Coin Change/Solution.java
+++ LeetCode/Java/Medium/322. Coin Change/Solution.java
@@ -1,17 +1,23 @@
 class Solution {
+    public int solve(int i,int t,int[] coins){
+        if(i==0){
+            if(t%coins[i]==0)return t/coins[i];
+            else return Integer.MAX_VALUE-1;
+        }
+        int tk=Integer.MAX_VALUE-1;
+        if(t>coins[i])
+        tk = 1+solve(i,t-coins[i],coins);
+        int nt = solve(i-1,t,coins);
+        return Math.min(tk,nt);
+
+
+    }
     public int coinChange(int[] coins, int V) {
         int n = coins.length;
-        int ans=0;
+        
         Arrays.sort(coins);
-  for (int i = n - 1; i >= 0; i--) {
-    while (V >= coins[i]) {
-      V -= coins[i];
-      ans++;
-    }
-    }
-    if(V==0)
-    return ans;
-    else return -1;
+        int res = solve(n-1,V,coins);
+        return res==Integer.MAX_VALUE-1?-1:res;
         
     }
 }
