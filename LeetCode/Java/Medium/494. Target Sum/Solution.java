ea60bb543c9e10b7167a531d334d1e0e47a833ef
Hrishabh Gupta <118746244+Hrishabh21@users.noreply.github.com>
2026-08-18 12:11:29 +0530

    Add Medium Java Solution from LeetCode

--- LeetCode/Java/Medium/494. Target Sum/Solution.java
+++ LeetCode/Java/Medium/494. Target Sum/Solution.java
@@ -1,43 +1,44 @@
 class Solution {
-    int sum;
+   
     public int solve(int[] nums,int t,int cs,int i,int[][] dp){
         if(i== nums.length){
             if(cs == t)
             return 1;
             return 0;
         }
-       int index = cs + sum;
+      // int index = cs + sum;
 
-        if (dp[i][index] != -1) {
-            return dp[i][index];
+        if (dp[i][cs] != -1) {
+            return dp[i][cs];
         }
+        int tk = solve(nums, t,cs+nums[i], i + 1, dp);
+        int nt = solve(nums, t,cs, i + 1, dp);
 
-        int add = solve(nums, t, cs + nums[i], i + 1, dp);
-        int subtract = solve(nums, t, cs - nums[i], i + 1, dp);
-
-        dp[i][index] = add + subtract;
-
-        return dp[i][index];
+        return dp[i][cs] = tk+nt;
 
     }
 
 
     public int findTargetSumWays(int[] nums, int target) {
-       // cnt = 0;
-        sum = 0;
+    
+        int sum = 0;
         int n = nums.length;
         for(int i:nums){
             sum+=i;
         }
-        
-        int dp[][] = new int[n][sum*2 + 2];
+        if (Math.abs(target) > sum) {
+            return 0;
+        }
+        if((sum-target)%2!=0) return 0;
+
+        int t = (sum-target)/2;
+        int dp[][] = new int[n][sum+1];
         for (int[] row : dp) {
             Arrays.fill(row, -1);
         }
-       // if(target<0)
-       // target = sum-target;
-        return solve(nums,target,0,0,dp);
-       // return cnt;
+       
+        return solve(nums,t,0,0,dp);
+       
 
 
         
