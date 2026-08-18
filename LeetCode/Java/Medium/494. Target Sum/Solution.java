5a947fe25c1b50447683ccebd6ca26cddc534e3e
Hrishabh Gupta <118746244+Hrishabh21@users.noreply.github.com>
2026-08-18 11:43:42 +0530

    Add Medium Java Solution from LeetCode

--- LeetCode/Java/Medium/494. Target Sum/Solution.java
+++ LeetCode/Java/Medium/494. Target Sum/Solution.java
@@ -1,22 +1,43 @@
 class Solution {
-    int cnt;
-    public void solve(int[] nums,int t,int cs,int i){
+    int sum;
+    public int solve(int[] nums,int t,int cs,int i,int[][] dp){
         if(i== nums.length){
             if(cs == t)
-            cnt++;
-            return;
+            return 1;
+            return 0;
         }
-        solve(nums,t,cs+nums[i],i+1);
-        solve(nums,t,cs-nums[i],i+1);
-        return;
+       int index = cs + sum;
+
+        if (dp[i][index] != -1) {
+            return dp[i][index];
+        }
+
+        int add = solve(nums, t, cs + nums[i], i + 1, dp);
+        int subtract = solve(nums, t, cs - nums[i], i + 1, dp);
+
+        dp[i][index] = add + subtract;
+
+        return dp[i][index];
 
     }
 
 
     public int findTargetSumWays(int[] nums, int target) {
-        cnt = 0;
-        solve(nums,target,0,0);
-        return cnt;
+       // cnt = 0;
+        sum = 0;
+        int n = nums.length;
+        for(int i:nums){
+            sum+=i;
+        }
+        
+        int dp[][] = new int[n][sum*2 + 2];
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+       // if(target<0)
+       // target = sum-target;
+        return solve(nums,target,0,0,dp);
+       // return cnt;
 
 
         
