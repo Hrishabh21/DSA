7c999f3d6be54cc78e9b1b66da46b39d0026103f
Hrishabh Gupta <118746244+Hrishabh21@users.noreply.github.com>
2026-08-22 12:59:22 +0530

    Add Java Solution to Patterns/Dynamic Programming

---
+++ LeetCode/Patterns/Dynamic Programming/516. Longest Palindromic Subsequence/Solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int longestPalindromeSubseq(String s) {
+        int n = s.length();
+        int prev[] = new int[n+1];
+        int curr[] = new int[n+1];
+        Arrays.fill(prev,0);
+        Arrays.fill(curr,0);
+        int max = 0;
+        for(int i =1;i<=n;i++){
+             Arrays.fill(curr,0);
+            for(int j =1;j<=n;j++){
+                if(s.charAt(i-1) == s.charAt(n-j)){
+                    curr[j] = prev[j-1]+1;
+                }
+                else{
+                    curr[j] = Math.max(curr[j-1],prev[j]);
+                }
+                //max = Math.max(max,curr[j]);
+            }
+            prev = curr.clone();
+           
+            
+        }
+        return curr[n];
+        
+    }
+}
