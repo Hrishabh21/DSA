4bad713324ab93dbdd8ad495cdec55e11b6e642e
Hrishabh Gupta <118746244+Hrishabh21@users.noreply.github.com>
2026-09-02 23:53:40 +0530

    Add Hard Java Solution for 115. Distinct Subsequences from LeetCode

--- LeetCode/Java/Hard/115. Distinct Subsequences/Solution.java
+++ LeetCode/Java/Hard/115. Distinct Subsequences/Solution.java
@@ -1,10 +1,10 @@
 class Solution {
     public int help(String s,String t,int i,int j,int[][]dp2){
 
-          if(j==0)return dp[1;
-          if(i==0) return 0;
+          if(j==0)return dp2[i][j] = 1;
+          if(i==0) return dp2[i][j] = 0;
         //  if(i==1&&j==1)return dp2[i][j] =1;
-          if(dp2[i][j]!=0) return dp2[i][j];;
+          if(dp2[i][j]!=-1) return dp2[i][j];;
          // dp2[i][j] = 1;
 
             if(s.charAt(i-1)==t.charAt(j-1)){
@@ -26,6 +26,9 @@ class Solution {
       
         
         int[][] dp2 = new int[n+1][m+1];
+        for(int[] d:dp2){
+            Arrays.fill(d,-1);
+        }
        
         return help(s,t,n,m,dp2);
 
