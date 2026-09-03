d842678577dcd35310e08186b7148d4813851377
Hrishabh Gupta <118746244+Hrishabh21@users.noreply.github.com>
2026-09-03 12:29:42 +0530

    Add Hard Java Solution for 44. Wildcard Matching from LeetCode

---
+++ LeetCode/Patterns/Dynamic Programming/44. Wildcard Matching/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean help(int i,int j,String s,String p){
+        if(i<0&&j<0) return true;
+        if(j<0) return false;
+        if(i<0&&p.charAt(j)=='*')
+        return help(i,j-1,s,p);
+        else if(i<0) return false;
+
+        if(s.charAt(i) == p.charAt(j)||p.charAt(j)=='?')
+        return help(i-1,j-1,s,p);
+        if(p.charAt(j)=='*'){
+            return help(i-1,j-1,s,p)|help(i-1,j,s,p);
+        }
+        return false;
+    }
+    public boolean isMatch(String s, String p) {
+        int m = s.length();
+        int n = p.length();
+        return help(m-1,n-1,s,p);
+        
+    }
+}
