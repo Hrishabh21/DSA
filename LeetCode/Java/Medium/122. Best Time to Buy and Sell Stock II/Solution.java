bcf6d8dd6e05c38bc6b6490aac0fc38def79e8bc
Hrishabh Gupta <118746244+Hrishabh21@users.noreply.github.com>
2026-09-04 12:25:57 +0530

    Add Medium Java Solution for 122. Best Time to Buy and Sell Stock II from LeetCode

--- LeetCode/Java/Medium/122. Best Time to Buy and Sell Stock II/Solution.java
+++ LeetCode/Java/Medium/122. Best Time to Buy and Sell Stock II/Solution.java
@@ -1,20 +1,19 @@
 class Solution {
-    public int help(int i,int cs,int[] prices,int n,int dp[][]){
+    public int help(int i,int cs,int[] prices,int n){
         if(i == n) return 0;
-        if(dp[i][cs+1]!=-1) return dp[i][cs+1];
+      //  if(dp[i][cs+1]!=-1) return dp[i][cs+1];
        
         //if dont hold any stock
         if(cs==-1||prices[i]<cs)
-        return dp[i][cs+1] = help(i+1,prices[i],prices,n,dp);
+        return help(i+1,prices[i],prices,n);
        
        int sell = 0;
-       int skip = 0;
+      
        //if hold any stock
        if(cs!=-1&&prices[i]>=cs)
-       sell =prices[i]-cs+ help(i+1,prices[i],prices,n,dp);
+       sell =prices[i]-cs+ help(i+1,prices[i],prices,n);
        
-        return dp[i][cs+1] = Math.max(sell,skip);
-
+        return sell;
     }
     public int maxProfit(int[] prices) {
         int n = prices.length;
@@ -22,10 +21,10 @@ class Solution {
         for(int p:prices){
             sum+=p;
         }
-        int dp[][] = new int[n][sum+2];
-        for(int i[]:dp)
-        Arrays.fill(i,-1);
-        return help(0,-1,prices,n,dp);
+       // int dp[][] = new int[n][sum+2];
+        // for(int i[]:dp)
+        // Arrays.fill(i,-1);
+        return help(0,-1,prices,n);
         
     }
 }
