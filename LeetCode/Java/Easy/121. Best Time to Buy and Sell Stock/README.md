84c643a3f0f701e5f2bf1541d1080e1886c32b44
Hrishabh Gupta <118746244+Hrishabh21@users.noreply.github.com>
2026-09-04 11:13:30 +0530

    Add Easy Java Solution for 121. Best Time to Buy and Sell Stock from LeetCode

--- LeetCode/Java/Easy/121. Best Time to Buy and Sell Stock/README.md
+++ LeetCode/Java/Easy/121. Best Time to Buy and Sell Stock/README.md
@@ -8,8 +8,8 @@
 Array, Dynamic Programming
 
 ### 🚀 Performance
-- **Runtime:** 2 ms
-- **Memory:** 94.7 MB
+- **Runtime:** N/A
+- **Memory:** N/A
 
 ---
 
--- LeetCode/Patterns/Dynamic Programming/121. Best Time to Buy and Sell Stock/Solution.java
+++ LeetCode/Patterns/Dynamic Programming/121. Best Time to Buy and Sell Stock/Solution.java
@@ -2,23 +2,23 @@ class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
         int mprof = 0;
-        int minVal = prices[0];
-        for(int i = 1;i<n;i++){
-            minVal= Math.min(minVal,prices[i]); 
-            mprof= Math.max(prices[i]-minVal,mprof); 
-        }
-
-    //     int [] minp = new int [n];
-    //   //  minp[0]=prices[0];
-    //     int [] maxp = new int [n];
-       // maxp[n-1]=prices[n-1];
+        // int minVal = prices[0];
         // for(int i = 1;i<n;i++){
-        //     minp[i] = Math.min(minp[i-1],prices[i]); 
-        //     maxp[n-1-i] = Math.max(maxp[n-i],prices[n-1-i]); 
+        //     minVal= Math.min(minVal,prices[i]); 
+        //     mprof= Math.max(prices[i]-minVal,mprof); 
         // }
-        // for(int i=0;i<n;i++){
-        //     mprof = Math.max(maxp[i]-minp[i],mprof);
-        // }
+
+        int [] minp = new int [n];
+        minp[0]=prices[0];
+        int [] maxp = new int [n];
+       maxp[n-1]=prices[n-1];
+        for(int i = 1;i<n;i++){
+            minp[i] = Math.min(minp[i-1],prices[i]); 
+            maxp[n-1-i] = Math.max(maxp[n-i],prices[n-1-i]); 
+        }
+        for(int i=0;i<n;i++){
+            mprof = Math.max(maxp[i]-minp[i],mprof);
+        }
         return mprof;  
     }
 }
