9015d920313ac2ba51ccbcea064e9808fcf1c40e
Hrishabh Gupta <118746244+Hrishabh21@users.noreply.github.com>
2026-09-04 12:22:44 +0530

    Add Medium Java Solution for 122. Best Time to Buy and Sell Stock II from LeetCode

--- LeetCode/Java/Medium/122. Best Time to Buy and Sell Stock II/Solution.java
+++ LeetCode/Java/Medium/122. Best Time to Buy and Sell Stock II/Solution.java
@@ -2,19 +2,18 @@ class Solution {
     public int help(int i,int cs,int[] prices,int n,int dp[][]){
         if(i == n) return 0;
         if(dp[i][cs+1]!=-1) return dp[i][cs+1];
-        int buy = 0;
+       
         //if dont hold any stock
-        if(cs==-1)
-        buy =help(i+1,prices[i],prices,n,dp);
+        if(cs==-1||prices[i]<cs)
+        return dp[i][cs+1] = help(i+1,prices[i],prices,n,dp);
        
        int sell = 0;
+       int skip = 0;
        //if hold any stock
        if(cs!=-1&&prices[i]>cs)
-       sell =prices[i]-cs+ help(i+1,-1,prices,n,dp);
-
-        //skip both buy/sell;
-        int skip = help(i+1,cs,prices,n,dp);
-        return dp[i][cs+1] =  Math.max(buy,Math.max(sell,skip));
+       sell =prices[i]-cs+ help(i+1,prices[i],prices,n,dp);
+       
+        return dp[i][cs+1] = Math.max(sell,skip);
 
     }
     public int maxProfit(int[] prices) {
