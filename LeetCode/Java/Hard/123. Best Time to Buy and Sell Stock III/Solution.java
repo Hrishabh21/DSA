8ad549967a5be1f809798c7dace0943c526d234a
Hrishabh Gupta <118746244+Hrishabh21@users.noreply.github.com>
2026-09-05 11:23:47 +0530

    Add Hard Java Solution for 123. Best Time to Buy and Sell Stock III from LeetCode

--- LeetCode/Java/Hard/123. Best Time to Buy and Sell Stock III/Solution.java
+++ LeetCode/Java/Hard/123. Best Time to Buy and Sell Stock III/Solution.java
@@ -1,17 +1,19 @@
 class Solution {
-    public int help(int i,boolean buy,int cap,int[] prices){
+    public int help(int i,int buy,int[][] dp,int cap,int[] prices){
         if(i == prices.length||cap == 0) return 0;
-        if(buy){
-            return Math.max(-prices[i]+help(i+1,false,cap,prices),help(i+1,buy,cap,prices));
+        if(dp[i][buy]!=-1) return dp[i][buy];
+        if(buy==1){
+            return dp[i][buy]=Math.max(-prices[i]+help(i+1,0,dp,cap,prices),help(i+1,buy,dp,cap,prices));
         }
         else{
-            return Math.max(prices[i]+help(i+1,true,cap-1,prices),help(i+1,buy,cap,prices));
+            return dp[i][buy]=Math.max(prices[i]+help(i+1,1,dp,cap-1,prices),help(i+1,buy,dp,cap,prices));
         }
 
     }
     public int maxProfit(int[] prices) {
        
-
-       return help(0,true,2,prices);
+        int dp[][] = new int[prices.length][2];
+        for(int[] d:dp) Arrays.fill(d,-1);
+        return help(0,1,dp,2,prices);
     }
 }
