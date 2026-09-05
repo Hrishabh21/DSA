2b003254b3ce236b24f525fb4477006b31425e8d
Hrishabh Gupta <118746244+Hrishabh21@users.noreply.github.com>
2026-09-05 11:32:52 +0530

    Add Hard Java Solution for 123. Best Time to Buy and Sell Stock III from LeetCode

--- LeetCode/Java/Hard/123. Best Time to Buy and Sell Stock III/Solution.java
+++ LeetCode/Java/Hard/123. Best Time to Buy and Sell Stock III/Solution.java
@@ -1,19 +1,23 @@
 class Solution {
-    public int help(int i,int buy,int[][] dp,int cap,int[] prices){
+    public int help(int i,int buy,int[][][] dp,int cap,int[] prices){
         if(i == prices.length||cap == 0) return 0;
-        if(dp[i][buy]!=-1) return dp[i][buy];
+        if(dp[i][buy][cap]!=-1) return dp[i][buy][cap];
         if(buy==1){
-            return dp[i][buy]=Math.max(-prices[i]+help(i+1,0,dp,cap,prices),help(i+1,buy,dp,cap,prices));
+            return dp[i][buy][cap]=Math.max(-prices[i]+help(i+1,0,dp,cap,prices),help(i+1,buy,dp,cap,prices));
         }
         else{
-            return dp[i][buy]=Math.max(prices[i]+help(i+1,1,dp,cap-1,prices),help(i+1,buy,dp,cap,prices));
+            return dp[i][buy][cap]=Math.max(prices[i]+help(i+1,1,dp,cap-1,prices),help(i+1,buy,dp,cap,prices));
         }
 
     }
     public int maxProfit(int[] prices) {
        
-        int dp[][] = new int[prices.length][2];
-        for(int[] d:dp) Arrays.fill(d,-1);
+        int dp[][][] = new int[prices.length][2][3];
+        for(int[][] d:dp){ 
+            for(int[] c:d){
+            Arrays.fill(c,-1);
+            }
+            }
         return help(0,1,dp,2,prices);
     }
 }
