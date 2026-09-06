bfb36a91e885c0a63cd857bded83390b686cdece
Hrishabh Gupta <118746244+Hrishabh21@users.noreply.github.com>
2026-09-06 12:11:53 +0530

    Add Medium Java Solution for 309. Best Time to Buy and Sell Stock with Cooldown from LeetCode

--- LeetCode/Java/Medium/309. Best Time to Buy and Sell Stock with Cooldown/Solution.java
+++ LeetCode/Java/Medium/309. Best Time to Buy and Sell Stock with Cooldown/Solution.java
@@ -12,12 +12,21 @@ class Solution {
     }
     public int maxProfit(int[] prices) {
        
-        int dp[][] = new int[prices.length][2];
-        for(int[] d:dp){
-            Arrays.fill(d,-1);
+        int dp[][] = new int[prices.length+2][2];
+        // for(int[] d:dp){
+        //     Arrays.fill(d,-1);
             
+        //     }
+        //return help(0,1,dp,prices);
+        for(int i = prices.length-1;i>=0;i--){      
+            dp[i][1]=Math.max(-prices[i]+dp[i+1][0],dp[i+1][1]);
+            
+        
+            dp[i][0]=Math.max(prices[i]+dp[i+2][1],dp[i+1][0]);
             }
-        return help(0,1,dp,prices);
+                
+    
 
+return dp[0][1];
    
 }}
