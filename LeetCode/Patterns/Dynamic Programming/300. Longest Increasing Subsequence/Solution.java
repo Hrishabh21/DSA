9f671ed7ebc743158c44950dff564bfc3ed35936
Hrishabh Gupta <118746244+Hrishabh21@users.noreply.github.com>
2026-09-08 10:57:19 +0530

    Add Medium Java Solution for 300. Longest Increasing Subsequence from LeetCode

--- LeetCode/Patterns/Dynamic Programming/300. Longest Increasing Subsequence/Solution.java
+++ LeetCode/Patterns/Dynamic Programming/300. Longest Increasing Subsequence/Solution.java
@@ -1,25 +1,38 @@
 class Solution {
- 
+    public int lis(int i,int prev,int[]nums,int[][] dp){
+        if(i==nums.length) return 0;
+        if(dp[i][prev+1]!=-1)return dp[i][prev+1];
+        int tk = 0;
+        
+        if(prev==-1||nums[i]>nums[prev]){
+            tk =1+lis(i+1,i,nums,dp);
+        }
+        
+        return dp[i][prev+1]= Math.max(tk,lis(i+1,prev,nums,dp));
+
+
+    }
     public int lengthOfLIS(int[] nums) {   
         int n = nums.length;
-      
-        int[] dp = new int[nums.length+1];
-        Arrays.fill(dp,1);
-         
-        int ans = 1;
-        for(int i = 1;i<n;i++){
-            for(int prev = i-1;prev>=0;prev--){
-               
-              if(nums[i]>nums[prev]){
-                    dp[i] = Math.max(dp[i],dp[prev]+1);
-                }       
+        int[][] dp = new int[nums.length+1][nums.length+1];
         
-            }
-            ans = Math.max(dp[i],ans);
+        // for(int[] d:dp){Arrays.fill(d,-1);}
+        // return lis(0,-1,nums,dp);
 
+        for(int i = n-1;i>=0;i--){
+            for(int prev = -1;prev<i;prev++){
+                 int tk = 0;
+        
+        if(prev==-1||nums[i]>nums[prev]){
+            tk =1+dp[i+1][i+1];
+        }
+        
+         dp[i][prev+1]= Math.max(tk,dp[i+1][prev+1]);
+
+            }
         }
 
-        return ans;
+        return dp[0][0];
         
     }
 }
