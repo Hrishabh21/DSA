0f5e3f9b454b179d99eecbd10026c5760e45f96a
Hrishabh Gupta <118746244+Hrishabh21@users.noreply.github.com>
2026-09-11 11:53:23 +0530

    Add Hard Java Solution for 1547. Minimum Cost to Cut a Stick from LeetCode

---
+++ LeetCode/Java/Hard/1547. Minimum Cost to Cut a Stick/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int helper(int[] cuts,int st,int end,int j,int n){
+        if(st>=end) return 0;
+        int ans = Integer.MAX_VALUE;
+       for(int i = st;i<end;i++){
+        int cut = (n-j)+helper(cuts,st,i,j,cuts[i])+helper(cuts,i+1,end,cuts[i],n);
+        ans = Math.min(ans,cut);
+
+       }
+       return ans;
+    }
+    public int minCost(int n, int[] cuts) {
+       
+        Arrays.sort(cuts);
+       
+       return helper(cuts,0,cuts.length,0,n);
+        
+    }
+}
