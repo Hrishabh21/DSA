a81aaa852f8b6e2636171b55605fe7b11fb937c8
Hrishabh Gupta <118746244+Hrishabh21@users.noreply.github.com>
2026-09-11 15:12:45 +0530

    Add Hard Java Solution for 312. Burst Balloons from LeetCode

--- LeetCode/Java/Hard/312. Burst Balloons/Solution.java
+++ LeetCode/Java/Hard/312. Burst Balloons/Solution.java
@@ -16,6 +16,22 @@ class Solution {
         ArrayList<Integer> arr = new ArrayList<>();
         for(int i:nums) arr.add(i);
         return help(arr);
+        int n = arr.length;
+        int[][] dp = new int[n][n];
+        for(int i =n-1;i>0;i--){
+            for(int j = i+1;j<n;j++){
+                int min = Integer.MAX_VALUE;
+       
+           for(int k = i;k<j;k++){
+               int steps = arr[i-1]*arr[k]*arr[j]+dp[i][k]+dp[k+1][j];
+               min = Math.min(min,steps);
+           }
+           dp[i][j]=min;
+            }
+        }
+        
+        
+        return dp[1][n-1];
 
         
     }
