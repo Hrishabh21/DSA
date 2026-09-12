c95a34d14444eabfa830c096725dd29186ea5b32
Hrishabh Gupta <118746244+Hrishabh21@users.noreply.github.com>
2026-09-12 10:33:34 +0530

    Add Hard Java Solution for 1197. Parsing A Boolean Expression from LeetCode

---
+++ LeetCode/Java/Hard/1197. Parsing A Boolean Expression/Solution.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean parseBoolExpr(String exp) {
+        Stack<Character> st  = new Stack<>();
+         Stack<Boolean> st2  = new Stack<>();
+        int i = 0;
+        int n = exp.length();
+        while(i<n){
+            if(st.isEmpty()){
+                st.push(exp.charAt(i));
+                i++;
+                continue;
+            }
+            char c = exp.charAt(i);
+            if(c ==')'){
+                while(st.peek()!='('){
+                    st2.add((st.pop()=='t'));
+
+                }
+                st.pop();
+                char ex = st.pop();
+                while(st2.size()>1){
+                    if(ex=='&'){
+                        st2.push(st2.pop()&st2.pop());
+                    }
+                    else{
+                        st2.push(st2.pop()|st2.pop());
+                    }
+                }
+                if(ex=='!')
+                st2.push(!st2.pop());
+                i++;
+                continue;
+
+            }
+            st.push(c);
+            i++;
+        }
+        return st2.pop();
+    }
+}
