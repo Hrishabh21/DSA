bd34109da73513c162cae7db891384617a31c6f5
Hrishabh Gupta <118746244+Hrishabh21@users.noreply.github.com>
2026-09-12 14:06:26 +0530

    Add Hard Java Solution for 132. Palindrome Partitioning II from LeetCode

--- LeetCode/Java/Hard/132. Palindrome Partitioning II/Solution.java
+++ LeetCode/Java/Hard/132. Palindrome Partitioning II/Solution.java
@@ -5,21 +5,12 @@ class Solution {
         
         if(pal[i][j-1]) return dp[i][j]=0;
       
-        int ans = Integer.MAX_VALUE;
+        int c = Integer.MAX_VALUE;
         for(int k =i+1;k<j;k++){
-             if (pal[i][k-1]) {
-
-                // s[i...k] is one palindrome
-                // Make one cut after it and solve remaining suffix
-                ans = Math.min(
-                    ans,
-                    1 + mc(s, k,j, dp, pal)
-                );
+            c =Math.min(c,mc(s,i,k,dp,pal)+mc(s,k,j,dp,pal));
         }
-       
+        return dp[i][j] = c+1;
 
-        }
-         return dp[i][j] = ans;
     }
     public int minCut(String s) {
         int n = s.length();
