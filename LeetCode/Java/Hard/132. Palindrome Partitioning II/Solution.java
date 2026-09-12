bf485ae221118478ecce3f266712c12fc10dd330
Hrishabh Gupta <118746244+Hrishabh21@users.noreply.github.com>
2026-09-12 14:06:46 +0530

    Add Hard Java Solution for 132. Palindrome Partitioning II from LeetCode

--- LeetCode/Java/Hard/132. Palindrome Partitioning II/Solution.java
+++ LeetCode/Java/Hard/132. Palindrome Partitioning II/Solution.java
@@ -10,16 +10,25 @@ class Solution {
      }
      return true;
     }
-    public int mc(String s,int i,int j){
-        if(isPalin(s.substring(i,j))) return 0;
+    public int mc(String s,int i,int j,int[][] dp){
+        if(isPalin(s.substring(i,j))) return dp[i][j]=0;
+        if(dp[i][j]!=-1)return dp[i][j];
         int c = Integer.MAX_VALUE;
         for(int k =i+1;k<j;k++){
-            c =Math.min(c,mc(s,i,k)+mc(s,k,j));
+            c =Math.min(c,mc(s,i,k,dp)+mc(s,k,j,dp));
         }
-        return c+1;
+        return dp[i][j] = c+1;
 
     }
     public int minCut(String s) {
-        return mc(s,0,s.length());
+        int n = s.length();
+
+
+
+        int[][] dp = new int[n][n+1];
+        for(int[] d:dp){
+            Arrays.fill(d,-1);
+        }
+        return mc(s,0,s.length(),dp);
     }
 }
