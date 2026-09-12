1cf5370741a2ca2c5cc8b730587afbabd2edf98c
Hrishabh Gupta <118746244+Hrishabh21@users.noreply.github.com>
2026-09-12 13:17:23 +0530

    Add Hard Java Solution for 132. Palindrome Partitioning II from LeetCode

---
+++ LeetCode/Patterns/Dynamic Programming/132. Palindrome Partitioning II/Solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isPalin(String s){
+     int n = s.length();
+     int i = 0;
+     while(i<n/2){
+        if(s.charAt(i)!=s.charAt(n-1-i)){
+            return false;
+        }
+        i++;
+     }
+     return true;
+    }
+    public int mc(String s,int i,int j){
+        if(isPalin(s.substring(i,j))) return 0;
+        int c = 0;
+        for(int k =i+1;k<j;k++){
+            c =Math.min(c,mc(s,i,k)+mc(s,k,j));
+        }
+        return c+1;
+
+    }
+    public int minCut(String s) {
+        return mc(s,0,s.length());
+    }
+}
