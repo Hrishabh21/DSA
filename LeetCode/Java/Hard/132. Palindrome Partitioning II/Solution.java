fa7f6314164bed2e7a2012f4f6231bb64d8e1d20
Hrishabh Gupta <118746244+Hrishabh21@users.noreply.github.com>
2026-09-12 14:20:18 +0530

    Add Hard Java Solution for 132. Palindrome Partitioning II from LeetCode

--- LeetCode/Java/Hard/132. Palindrome Partitioning II/Solution.java
+++ LeetCode/Java/Hard/132. Palindrome Partitioning II/Solution.java
@@ -1,45 +1,41 @@
 class Solution {
-   
-    public int mc(String s,int i,int j,int[][] dp,boolean[][] pal){
-        if(dp[i][j]!=-1)return dp[i][j];
-        
-        if(pal[i][j-1]) return dp[i][j]=0;
-      
-        int ans = Integer.MAX_VALUE;
-        for(int k =i+1;k<j;k++){
-             if (pal[i][k-1]) {
-
-                // s[i...k] is one palindrome
-                // Make one cut after it and solve remaining suffix
-                ans = Math.min(
-                    ans,
-                    1 + mc(s, k,j, dp, pal)
-                );
+    public boolean isPalin(String s){
+     int n = s.length();
+     int i = 0;
+     while(i<n/2){
+        if(s.charAt(i)!=s.charAt(n-1-i)){
+            return false;
         }
-       
-
+        i++;
+     }
+     return true;
+    }
+    public int mc(String s,int i,int[] dp){
+        if(i==s.length())return 0;
+        if(dp[i]!=-1)return dp[i];
+        int c = Integer.MAX_VALUE;
+        for(int k =i+1;k<=s.length();k++){
+            if(isPalin(s.substring(i,k))){
+                if (k == s.length()) {
+                c = Math.min(c, 0);
+            } else {
+                c = Math.min(c, 1 + mc(s, k, dp));
+            }
+            }
+         
         }
-         return dp[i][j] = ans;
+        return dp[i] = c;
+
     }
     public int minCut(String s) {
         int n = s.length();
 
-        boolean[][] pal = new boolean[n][n];
 
-        for (int i = n - 1; i >= 0; i--) {
-            for (int j = i; j < n; j++) {
 
-                if (s.charAt(i) == s.charAt(j) &&
-                    (j - i <= 2 || pal[i + 1][j - 1])) {
-
-                    pal[i][j] = true;
-                }
-            }
-        }
-        int[][] dp = new int[n][n+1];
-        for(int[] d:dp){
-            Arrays.fill(d,-1);
-        }
-        return mc(s,0,s.length(),dp,pal);
+        int[]dp = new int[n];
+       
+            Arrays.fill(dp,-1);
+        
+        return mc(s,0,dp);
     }
 }
