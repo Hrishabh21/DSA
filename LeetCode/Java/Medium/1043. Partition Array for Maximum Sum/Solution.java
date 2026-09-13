b68ecedd7e5164e869eb1401fdee80e7743a9c48
Hrishabh Gupta <118746244+Hrishabh21@users.noreply.github.com>
2026-09-13 11:16:08 +0530

    Add Medium Java Solution for 1043. Partition Array for Maximum Sum from LeetCode

--- LeetCode/Java/Medium/1043. Partition Array for Maximum Sum/Solution.java
+++ LeetCode/Java/Medium/1043. Partition Array for Maximum Sum/Solution.java
@@ -1,18 +1,21 @@
 class Solution {
-    public int help(int i,int j,int[] arr, int k){
-        if(i>j) return 0;
+    public int help(int i,int[] arr, int k,int[] dp){
+        int n= arr.length;
+        if(i>=n) return 0;
+        if(dp[i]!=-1)return dp[i];
         int max = arr[i];
         int ans = Integer.MIN_VALUE;
-        for(int l = i;(l<=j)&&(l<i+k);l++){
+        for(int l = i;(l<n)&&(l<i+k);l++){
             max = Math.max(arr[l],max);
-            ans = Math.max(ans,max*(l-(i-1))+help(l+1,j,arr,k));
+            ans = Math.max(ans,max*(l-(i-1))+help(l+1,arr,k,dp));
         }
-        return ans;
+        return dp[i] = ans;
     }
     public int maxSumAfterPartitioning(int[] arr, int k) {
         int n = arr.length;
-        i
-        return help(0,n-1,arr,k);
+        int dp[] = new int[n];
+        Arrays.fill(dp,-1);
+        return help(0,arr,k,dp);
 
         
     }
