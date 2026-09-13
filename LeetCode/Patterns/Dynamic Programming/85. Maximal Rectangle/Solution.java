08c82f5f4d5eeadf0fe24a5108a756df34fd10f8
Hrishabh Gupta <118746244+Hrishabh21@users.noreply.github.com>
2026-09-13 12:17:09 +0530

    Add Hard Java Solution for 85. Maximal Rectangle from LeetCode

--- LeetCode/Patterns/Dynamic Programming/85. Maximal Rectangle/Solution.java
+++ LeetCode/Patterns/Dynamic Programming/85. Maximal Rectangle/Solution.java
@@ -1,39 +1,64 @@
 class Solution {
-    public int largestRect(int[] bar){
-        int n = bar.length;
+
+     public int LRA(int[] heights) {
         Stack<Integer> st = new Stack<>();
-        st.push(-1);
-        int ans = 0;
-        for(int i = 0;i<n;i++){
-            while(st.size()>1&&bar[st.peek()]>=bar[i]){
-                int temp = bar[st.pop()]*(i - st.peek()-1);
-                ans = Math.max(ans,temp);
+        int n = heights.length;
+        int[] nse = new int[n];
+      
+        for(int i=n-1;i>=0;i--){
+            while(!st.isEmpty()&&heights[i]<=heights[st.peek()]){
+                st.pop();
+            }
+            if(st.isEmpty()){
+                nse[i] = n;
+            }
+            else{
+                nse[i] = st.peek();
             }
             st.push(i);
         }
-         while(st.size()>1){
-                int temp = bar[st.pop()]*(n - st.peek()-1);
-                ans = Math.max(ans,temp);
+        st.clear();
+        int ans = 0,pse;
+        for(int i = 0;i<n;i++){
+             while(!st.isEmpty()&&heights[i]<=heights[st.peek()]){
+                st.pop();
             }
+            if(st.isEmpty()){
+                pse = -1;
+            }
+            else{
+                pse = st.peek();
+            }
+            st.push(i);
+            int temp = heights[i]*(nse[i]-pse-1);
+            ans = Math.max(temp,ans);
+        }
         return ans;
 
+
     }
+
     public int maximalRectangle(char[][] matrix) {
-        int [][] bars = new int[matrix.length][matrix[0].length];
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int[] prev = new int[m];
+        Arrays.fill(prev,0);
+        int curr[] = new int[m];
+       
         int ans = 0;
-        for(int i = 0;i<matrix.length;i++){
-            for(int j = 0;j<matrix[0].length;j++){
-                if(i==0)
-                bars[i][j] = matrix[i][j]-'0';
-                else{
-                   if(matrix[i][j]=='1'){
-                    bars[i][j] = bars[i-1][j]+1;
-                   } 
-                   else
-                    bars[i][j]=0;
+        for(int i =0;i<n;i++){
+             Arrays.fill(curr,0);
+            for(int j = 0;j<m;j++){
+                if(matrix[i][j]=='1'){
+                    curr[j] = 1+prev[j];
                 }
+                prev[j] = curr[j];
+
             }
-            ans = Math.max(ans,largestRect(bars[i]));
+            ans = Math.max(ans,LRA(curr));
+           
+
+
 
         }
         return ans;
